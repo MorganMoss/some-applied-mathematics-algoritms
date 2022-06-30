7ffd028ea66445d0946bbb41cd40f82c15ae735a
MorganMoss <raveressmoss@gmail.com>
2022-06-30 19:29:30 +0200

    neatened code; improved scaling in graph window

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import math
 
-# import window
-
 x_list = [-1, -0.5, 0, 0.5]
 fx_list = [ 0.86199480, 0.95802009, 1.0986123, 1.2943767]
 
 value_set:set = set()
 
-control_points = list(map(lambda a, b : (a,b), x_list, fx_list))
+printout = False
 
+#Actual curve
 def actual(x):
-    print("Actual value: ")
+    if printout: print("Actual value: ")
     f_x = math.log((math.e**x)+2)
-    print(f"f({x}) = {f_x}\n")
+    if printout: print(f"f({x}) = {f_x}\n")
     return f_x
 
-
+#Lagrange Interpolating Polynomial
 def lagrange_interpolating_polynomial(approx_x):
     n = len(x_list)
     p_x = 0
-    print(f"Lagrange interpolating polynomial of degree {n-1}")
+    if printout: print(f"Lagrange interpolating polynomial of degree {n-1}")
 
     #P(x) sum 
     for j in range(n):  
@@ -30,14 +29,14 @@ def lagrange_interpolating_polynomial(approx_x):
             if j == k : continue
             product *= (approx_x-x_list[k])/(x_list[j]-x_list[k])
         product *= fx_list[j]
-        print(f"P{j+1}({approx_x}) = {product}")
+        if printout: print(f"P{j+1}({approx_x}) = {product}")
 
         p_x += product
 
-    print(f"P({approx_x}) = {p_x}\n")
+    if printout: print(f"P({approx_x}) = {p_x}\n")
     return p_x
 
-
+#Used for Newtons Divided Difference Formula
 def divided_difference(list_x, list_y):
     if len(list_x) == 2:
         value = (list_y[1]-list_y[0])/(list_x[1]-list_x[0])
@@ -52,8 +51,10 @@ def divided_difference(list_x, list_y):
     global value_set
     value_set.add(f"f{list_x} = {value}")
     return value
+
+#Newtons Divided Difference Formula
 def newtons_divided_difference_formula(approx_x):
-    print(f"Newtons divided difference formula of degree {len(x_list)-1}")
+    if printout: print(f"Newtons divided difference formula of degree {len(x_list)-1}")
     p_x = fx_list[0]
 
     for n in range(1, len(fx_list)):
@@ -73,37 +74,37 @@ def newtons_divided_difference_formula(approx_x):
     global value_set
     value_set = list(value_set)
     value_set.sort(key = lambda a: len(a))
-    print("Table of results:")
-    print(*value_set, sep="\n")
+    if printout: print("Table of results:")
+    if printout: print(*value_set, sep="\n")
     value_set = set()
 
 
-    print(f"P({approx_x}) = {p_x}")
+    if printout: print(f"P({approx_x}) = {p_x}")
     return p_x
 
-
+#Just gets the first one - fine for my use case
 def get_roots(x):
-    a = -control_points[0][0]+3*control_points[1][0]-3*control_points[2][0]+control_points[3][0]
-    b = 3*control_points[0][0] - 6*control_points[1][0] + 6*control_points[2][0]
-    c = -3*control_points[0][0] + 3*control_points[1][0]
-    d = control_points[0][0]-x
+    a = -x_list[0]+3*x_list[1]-3*x_list[2]+x_list[3]
+    b = 3*x_list[0] - 6*x_list[1] + 6*x_list[2]
+    c = -3*x_list[0] + 3*x_list[1]
+    d = x_list[0]-x
 
     if a == 0 and b == 0 and c == 0:
         t = -d
-        print(f"t at {x} = {t}")
+        if printout: print(f"t at {x} = {t}")
         return t
 
 
     if a == 0 and b == 0:
         t = -d/c
-        print(f"t at {x} = {t}")
+        if printout: print(f"t at {x} = {t}")
         return t
 
 
     if a == 0: 
         a,b,c = b,c,d
         t = (-b + (b**2-4*a*c)**(1/2))/(2*a)
-        print(f"t at {x} = {t}")
+        if printout: print(f"t at {x} = {t}")
         return t
 
 
@@ -119,14 +120,16 @@ def get_roots(x):
         C *= ((-1+(-3)**(1/2))/2)**0
         t = (-1/(3*a)*(b + C + del0/C))
 
-    print(f"t at {x} = {t}")
+    if printout: print(f"t at {x} = {t}")
     return t
 
-
+#Cubic Bezier Curve 
+#  General Formula was a lot to implement when considering
+#  I want to find a t that gives me x = 0.25 - which involves solving a nth-degree polynomial
 def bezier_point(approx_x):  
     control_points = list(map(lambda a, b : (a,b), x_list, fx_list))
 
-    print(f"Bezier point from control points: {control_points}")  
+    if printout: print(f"Bezier point from control points: {control_points}")  
     t = get_roots(approx_x)
 
     x = (
@@ -143,25 +146,27 @@ def bezier_point(approx_x):
             + t**3*control_points[3][1]
         )   
     
-    print(f"B({t}) = {(x,y)}")
+    if printout: print(f"B({t}) = {(x,y)}")
     return (x,y)
 
-def find_specific_x_from_bezier():
-    ...
+#4th derivative of f
+# def f4(x):
+#     return (2*math.e*x*(math.e^(2*x)-8*math.e**x+4))/(math.e**x + 2)**4
 
-
-def absolute_error(actual, approx):
-    Ea = actual - approx
-    min(*x_list)
-    Ea
-    print(f"absolute error = {Ea}\n")
+def actual_error(actual, approx):
+    error = abs(actual - approx)
+    if printout: print(f"Error = {error}\n")
 
 
 def main():
     f_x = actual(0.25)
-    absolute_error(f_x, lagrange_interpolating_polynomial(0.25))
-    absolute_error(f_x, newtons_divided_difference_formula(0.25))
-    absolute_error(f_x, bezier_point(0.25)[1])
+    global printout
+    printout = True
+    #Question 1.1-1.2
+    actual_error(f_x, lagrange_interpolating_polynomial(0.25))
+    actual_error(f_x, newtons_divided_difference_formula(0.25))
+    actual_error(f_x, bezier_point(0.25)[1])
+    #Question 2.1-2.3
 
-
-main()
+if __name__ == '__main__': 
+    main()
--- window.py
+++ window.py
@@ -1,102 +1,150 @@
-import main
 import pyglet
 from pyglet import shapes
- 
-title = "Graphs"
-width = 900
-height = 800
 
-window = pyglet.window.Window(width, height, title)
-batch = pyglet.graphics.Batch()
+import main
 
 
 
-line_width = 1
-y_offset = 225
-
- 
-x_axis = shapes.Line(0, height/2-y_offset, width, height/2-y_offset,
-    line_width, color = (255, 225, 255), batch = batch)
-y_axis = shapes.Line(width/2, 0, width/2, height,
-    line_width, color = (255, 225, 255), batch = batch)
-
-lines = list()
-scale = 300
-
-line_width = 1
-for x in range(width):
-    x-=width/2
-    prev_x = x -1
-
-    y = main.actual(x/scale)*scale-y_offset
-    prev_y = main.actual(prev_x/scale)*scale-y_offset
-    lines += [shapes.Line(prev_x+width/2, prev_y+height/2, x+width/2, y+height/2,
-        line_width, color = (255, 255, 255), batch = batch)]
-
-    y = main.lagrange_interpolating_polynomial(x/scale)*scale-y_offset
-    prev_y = main.lagrange_interpolating_polynomial(prev_x/scale)*scale-y_offset
-
-    lines += [shapes.Line(prev_x+width/2, prev_y+height/2, x+width/2, y+height/2,
-        line_width, color = (255, 0, 0), batch = batch)]
-
-    y = main.newtons_divided_difference_formula(x/scale)*scale-y_offset
-    prev_y = main.newtons_divided_difference_formula(prev_x/scale)*scale-y_offset
-
-    lines += [shapes.Line(prev_x+width/2, prev_y+height/2, x+width/2, y+height/2,
-        line_width, color = (0, 255, 0), batch = batch)]
- 
-    x,y = main.bezier_point(x/scale)
-    x,y = x*scale, y*scale-y_offset
-    prev_x,prev_y = main.bezier_point(prev_x/scale)
-    prev_x,prev_y = prev_x*scale, prev_y*scale-y_offset
-
-    lines += [shapes.Line(prev_x+width/2, prev_y+height/2, x+width/2, y+height/2,
-        line_width, color = (0, 0, 255), batch = batch)]
+to_draw = list()
 
 
 
+def draw_axis(line_width = 1):
+    global to_draw
+    x_axis = shapes.Line(0, height/2-y_offset, width, height/2-y_offset,
+        line_width, color = (255, 225, 255), batch = batch)
+    y_axis = shapes.Line(width/2, 0, width/2, height,
+        line_width, color = (255, 225, 255), batch = batch)
+    to_draw += [x_axis,  y_axis]
 
-line_width = 3
-x_ls = [-1, -0.5, 0, 0.25, 0.5]
 
-def make_point(x_, y_, color_, offset = 20):
-    global lines
+def draw_graphs(line_width = 1):
+    global to_draw
+    for x in range(width):
+        x-=width/2
+        prev_x = x -1
+
+        y = main.actual(x/scale)*scale-y_offset
+        prev_y = main.actual(prev_x/scale)*scale-y_offset
+        to_draw += [shapes.Line(prev_x+width/2, prev_y+height/2, x+width/2, y+height/2,
+            line_width, color = (255, 255, 255), batch = batch)]
+
+        y = main.lagrange_interpolating_polynomial(x/scale)*scale-y_offset
+        prev_y = main.lagrange_interpolating_polynomial(prev_x/scale)*scale-y_offset
+
+        to_draw += [shapes.Line(prev_x+width/2, prev_y+height/2, x+width/2, y+height/2,
+            line_width, color = (255, 0, 0), batch = batch)]
+
+        y = main.newtons_divided_difference_formula(x/scale)*scale-y_offset
+        prev_y = main.newtons_divided_difference_formula(prev_x/scale)*scale-y_offset
+
+        to_draw += [shapes.Line(prev_x+width/2, prev_y+height/2, x+width/2, y+height/2,
+            line_width, color = (0, 255, 0), batch = batch)]
+    
+        x,y = main.bezier_point(x/scale)
+        x,y = x*scale, y*scale-y_offset
+        prev_x,prev_y = main.bezier_point(prev_x/scale)
+        prev_x,prev_y = prev_x*scale, prev_y*scale-y_offset
+
+        to_draw += [shapes.Line(prev_x+width/2, prev_y+height/2, x+width/2, y+height/2,
+            line_width, color = (0, 0, 255), batch = batch)]
+
+
+def make_point(x_, y_, color_, offset = 20, line_width = 3):
+    global to_draw
     r = 3
-    lines += [shapes.Circle(x_+width/2, y_+height/2,line_width,color=color_,batch = batch)]
+    to_draw += [shapes.Circle(x_+width/2, y_+height/2,line_width,color=color_,batch = batch)]
     color_ = (*color_,255)
-    lines += [pyglet.text.Label(text = f"({round(x_/scale, r)},{round((y_-y_offset)/scale, r)})",
+    to_draw += [pyglet.text.Label(text = f"({round(x_/scale, r)},{round((y_-y_offset)/scale, r)})",
         font_name='Times New Roman',
-        font_size=8,
-        x=x_+width/2,
+        font_size=global_font_size,
+        x=x_+width/2+2,
         y=y_+height/2+offset, 
         color = color_,
-        # anchor_x='center',
-        anchor_y='center', batch=batch)]
-
-for x in x_ls:
-    # x-=width/2
-    x*=scale
-
-    y = main.actual(x/scale)*scale-y_offset
-    make_point(x, y, (255, 255, 255), 60)
-
-    y = main.lagrange_interpolating_polynomial(x/scale)*scale-y_offset
-    make_point(x, y, (255, 0,0), 40)
-
-    y = main.newtons_divided_difference_formula(x/scale)*scale-y_offset
-    make_point(x, y, (0, 255, 0))
-
-    x,y = main.bezier_point(x/scale)
-    x,y = x*scale, y*scale-y_offset
-    make_point(x, y, (100,100,255), -20)
+        anchor_y='center', batch=text_batch)]
 
 
-# window draw event
-@window.event
-def on_draw():
-     
-    window.clear()
-     
-    batch.draw()
+def plot_points( x_ls = [-1, -0.5, 0, 0.25, 0.5]):
+    for x in x_ls:
+        x*=scale
+
+        y = main.actual(x/scale)*scale-y_offset
+        make_point(x, y, (255, 255, 255), 60)
+
+        y = main.lagrange_interpolating_polynomial(x/scale)*scale-y_offset
+        make_point(x, y, (255, 0,0), 40)
+
+        y = main.newtons_divided_difference_formula(x/scale)*scale-y_offset
+        make_point(x, y, (0, 255, 0))
+
+        x,y = main.bezier_point(x/scale)
+        x,y = x*scale, y*scale-y_offset
+        make_point(x, y, (100,100,255), -20)
+
+
+def label():
+    global to_draw
+
+    g_list = [
+        ("Actual", (255,255,255,255)),
+        ("Lagrange Interpolating Polynomial", (255, 0,0,255)),
+        ("Newtons Divided Difference Formula", (0, 255, 0,255)),
+        ("Bezier Curve", (100,100,255,255))
+    ]
+
+    i = 0
+
+    for g in g_list:
+        name, color = g
+        to_draw += [pyglet.text.Label(text = f"{name}  ⎯⎯⎯⎯⎯⎯",
+            font_name='Times New Roman',
+            font_size=global_font_size,
+            x=5,
+            y=height-10-i*global_font_size*2.5, 
+            color = color,
+            anchor_y='center', batch=text_batch)]
+        i+=1
+
+
+if __name__ == "__main__":
+    display = pyglet.canvas.Display()
+    screen = display.get_default_screen()
+    width = screen.width
+    height = screen.height
+
+
+    scale = input("enter the scale (Enter for default) : ")
+    if scale == "": 
+        scale = max(height, width)/3
+    else: 
+        scale = int(scale)
+
+    y_offset = input("enter the y offset (Enter for default) : ")
+    if y_offset == "": 
+        y_offset = scale/3*4
+    else: 
+        y_offset = int(y_offset)
+
+
+    window = pyglet.window.Window(width, height, "Graphs")
+    batch = pyglet.graphics.Batch()
+    text_batch = pyglet.graphics.Batch()
+
+    global_font_size = 12
+
+    
+    draw_axis()
+    draw_graphs()
+    plot_points()
+    label()
+
+    @window.event
+    def on_draw():
+        window.clear()
+        batch.draw()
+        text_batch.draw()
  
-pyglet.app.run()
+    pyglet.app.run()
+
+    main.main()
+
